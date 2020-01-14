c38c083b329b25136e350300123e957826c095ad
root <aayeh@chapman.edu>
2020-01-14 20:53:30 +0000

    I made additional changes

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-  cout << "Hello world!";
+  cout << "Hello world!" << endl;
+  cout << "I had to add this statement because Rene said so";
+  cout << "adding another cout" << ;
 
   return 0;
 }
